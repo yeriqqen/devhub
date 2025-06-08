50a914129b1692a206bf3169e1f4860518473c83
Yestay <yestayyessenbay@gmail.com>
2025-06-08 23:12:07 +0900

    chore: Update tailwind config, pathways, Update pnpm dependencies

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -5,6 +5,10 @@ export default {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/widgets/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/features/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/entities/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/shared/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
