27eaa8f3e7944bc9c55f56a1e54de5407042b52e
Yestay <yestayyessenbay@gmail.com>
2025-06-09 12:51:25 +0900

    fix: Update next.config.ts and vercel.json

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  distDir: ".next",
+  trailingSlash: false,
 };
 
 export default nextConfig;
