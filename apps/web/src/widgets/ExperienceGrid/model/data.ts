fda10d06dbde03623635474f8c7e849bde78e03a
Yestay <yestayyessenbay@gmail.com>
2026-07-10 20:54:31 +0900

    chore: Update ExperienceGrid data

--- apps/web/src/widgets/ExperienceGrid/model/data.ts
+++ apps/web/src/widgets/ExperienceGrid/model/data.ts
@@ -5,22 +5,11 @@ export const defaultExperiences: Experience[] = [
     id: "dep",
     name: "DEP",
     role: "Full Stack Developer",
-    duration: "2025 - Present",
+    duration: "2025 - 2026",
     description:
       "Led development of enterprise-grade web applications and implemented scalable cloud solutions for Fortune 500 clients.",
     image1: "/experiences/dep/dep.jpeg",
     video: "/experiences/dep/VSS_RECAP_2023.mp4",
     link: "https://dep.team",
-  },
-  {
-    id: "vetra",
-    name: "Vetra AI",
-    role: "Startup CTO",
-    duration: "2025 - Present",
-    description:
-      "Led development of enterprise-grade web applications and implemented scalable cloud solutions for Fortune 500 clients.",
-    image1: "/experiences/vetra/vetra_1.png",
-    image2: "/experiences/vetra/vetra_2.png",
-    link: "https://vetra.ai",
   },
 ];
